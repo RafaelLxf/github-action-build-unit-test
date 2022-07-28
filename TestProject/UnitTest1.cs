2a28560a8cd548da5319727f926ace9c03d6ee6d
lanxiaofang <13604461032@163.com>
2022-07-29 01:41:29 +0800

    change files

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+using TestLib;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            PrintClass.PrintHello();
+        }
+    }
+}
