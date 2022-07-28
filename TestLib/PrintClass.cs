33c8545c4c2fa8f1ac34d2395a43943960267673
lanxiaofang <13604461032@163.com>
2022-07-28 19:06:33 +0800

    first commit

---
+++ TestLib/PrintClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    public class PrintClass
+    {
+        public static void PrintHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
